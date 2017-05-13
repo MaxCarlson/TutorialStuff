0920ec4d07ba784475292779ba6e6a31b0eeb6f2
Maxwell Carlson <bart4282819@gmail.com>
2017-05-12 19:23:09 -0700

    more strings stuff

--- stringSorting.cpp
+++ stringSorting.cpp
@@ -22,3 +22,34 @@ std::vector<std::string> allLongestStrings(std::vector<std::string> inputArray) 
     
     
 }
+
+
+
+int commonCharacterCount(std::string s1, std::string s2) {
+
+    int len, len1;
+    if(s1.size()>s2.size()){
+        len = s1.size();
+        len1 = s2.size();
+    } else {
+        len = s2.size();
+        len1 = s1.size();
+    }
+    
+    int counter = 0;
+    
+    for(int i = 0; i < len; i++){
+         for(int j = 0; j < len1; j++){
+             if(s1[i] == s2[j] && s1[i]!= NULL && s2[j] != NULL){
+                 counter ++;
+                 s1.erase(s1.begin()+i);
+                 s2.erase(s2.begin()+j);
+                 i -= 1;
+                 j -= 1;
+             }
+         }
+    }
+     
+return counter;   
+ 
+}
